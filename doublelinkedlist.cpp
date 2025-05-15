a8a1fe54272771aea5b5922bc01ae6623f7e599a
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-15 19:12:16 +0700

    Step 1

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -89,5 +89,14 @@ public:
         cin >> rollNo;
 
         Node *current = START;
+
+        //  Step 1: Traverse the list to find th enode
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };
