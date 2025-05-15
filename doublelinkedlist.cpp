9afc8ccbf60b1153ed92f2ecfff7c630e0e4b2b9
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-15 18:56:57 +0700

    Step 1

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -25,5 +25,8 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        // Step 1 : Allocate memory for new node
+        Node *newNode = new Node();
     }
 };
