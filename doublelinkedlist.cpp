2d91a824804fd36d35414949143739151d1982d8
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-15 18:53:43 +0700

    Membuat Class DoubleLinkedList

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -9,3 +9,21 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
+};
